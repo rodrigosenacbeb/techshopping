7cea61d6afa49c413a39d94e4fd460229937496b
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-11-13 09:47:43 -0300

    arquivo de conexao com o banco de dados

---
+++ Controller/AcessoDadosSqlServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class AcessoDadosSqlServer
+    {
+        // String de conexão com o banco de dados.
+        string parametrosConexao = string.Format(@"Data Source={0}; Initial Catalog={1};User ID={2}; Password={3}",
+            Properties.Settings.Default.ServidorBanco,
+            Properties.Settings.Default.NomeBanco,
+            Properties.Settings.Default.UsuarioBanco,
+            Properties.Settings.Default.SenhaBanco);
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection conexao = new SqlConnection(parametrosConexao);
+
+            try
+            {                
+                conexao.Open();
+                return conexao;
+            }
+            catch 
+            {
+                conexao.Close();
+                throw new Exception();                
+            }
+        }
+
+        public void Fechar(SqlConnection conexao)
+        {
+            conexao.Close();
+        }
+
+    }
+}
